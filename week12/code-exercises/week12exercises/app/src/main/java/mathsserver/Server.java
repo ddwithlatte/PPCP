ae39e6e4f993177aa87aa40eb9efe4551329e19b
ddwithlatte <93472089+ddwithlatte@users.noreply.github.com>
2022-12-05 16:04:08 +0100

    Add files via upload

---
+++ week12/code-exercises/week12exercises/app/src/main/java/mathsserver/Server.java
@@ -0,0 +1,119 @@
+package mathsserver;
+
+// Hint: The imports below may give you hints for solving the exercise.
+//       But feel free to change them.
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.ChildFailed;
+import akka.actor.typed.Terminated;
+import akka.actor.typed.javadsl.*;
+import scala.Tuple2;
+
+import java.util.*;
+
+public class Server extends AbstractBehavior<Server.ServerCommand> {
+    /* --- Messages ------------------------------------- */
+    public interface ServerCommand { }
+
+    public static final class ComputeTasks implements ServerCommand {
+        public final List<Task> tasks;
+        public final ActorRef<Client.ClientCommand> client;
+
+        public ComputeTasks(List<Task> tasks, ActorRef<Client.ClientCommand> client) {
+            this.tasks  = tasks;
+            this.client = client;
+        }
+    }
+
+    public static final class WorkDone implements ServerCommand {
+        ActorRef<Worker.WorkerCommand> worker;
+
+        public WorkDone(ActorRef<Worker.WorkerCommand> worker) {
+            this.worker = worker;
+        }
+    }
+
+    /* --- State ---------------------------------------- */
+    private int workerIds = 0;
+    private int totalWorkers;// 
+    private int maxWorkers;
+    private Queue<ActorRef<Worker.WorkerCommand>> idleWorkers = new LinkedList<>();
+    private HashSet<ActorRef<Worker.WorkerCommand>> busyWorkers = new HashSet<>();
+    private Queue<Tuple2<ActorRef<Client.ClientCommand>, Task>> tasks = new LinkedList<>();
+
+
+
+    /* --- Constructor ---------------------------------- */
+    private Server(ActorContext<ServerCommand> context, int minWorkers, int maxWorkers) {
+        super(context);
+        this.totalWorkers = minWorkers;
+        this.maxWorkers = maxWorkers;
+        for(int i = 0; i < minWorkers; i++) {
+            ActorRef<Worker.WorkerCommand> worker = context.spawn(Worker.create(context.getSelf()), "worker_"+(workerIds++));
+            getContext().watch(worker);
+            idleWorkers.add(worker);
+        }
+    }
+
+
+    /* --- Actor initial state -------------------------- */
+    public static Behavior<ServerCommand> create(int minWorkers, int maxWorkers) {
+        return Behaviors.setup(context -> new Server(context, minWorkers, maxWorkers));
+    }
+
+
+    /* --- Message handling ----------------------------- */
+    @Override
+    public Receive<ServerCommand> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(ComputeTasks.class, this::onComputeTasks)
+                .onMessage(WorkDone.class, this::onWorkDone)
+                .onSignal(ChildFailed.class, this::onCrash)
+                .build();
+    }
+
+
+    /* --- Handlers ------------------------------------- */
+    public Behavior<ServerCommand> onComputeTasks(ComputeTasks msg) {
+        for (Task t : msg.tasks) {
+            if (!idleWorkers.isEmpty()) {
+                ActorRef<Worker.WorkerCommand> worker = idleWorkers.poll();
+                busyWorkers.add(worker);
+                worker.tell(new Worker.ComputeTask(t, msg.client));
+            }
+            else if (totalWorkers < maxWorkers) {
+                totalWorkers++;
+                ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()), "worker"+(workerIds++));
+                getContext().watch(worker);
+                busyWorkers.add(worker);
+                worker.tell(new Worker.ComputeTask(t, msg.client));
+            }
+            else {
+                tasks.add(new Tuple2<ActorRef<Client.ClientCommand>,Task>(msg.client, t));
+            }
+        }
+        return this;
+    }
+
+    public Behavior<ServerCommand> onWorkDone(WorkDone msg) {
+        if (!busyWorkers.contains(msg.worker)) return this;
+        if (!tasks.isEmpty()) {
+            Tuple2<ActorRef<Client.ClientCommand>, Task> task = tasks.poll();
+            msg.worker.tell(new Worker.ComputeTask(task._2, task._1()));
+        }
+        else {
+            busyWorkers.remove(msg.worker);
+            idleWorkers.add(msg.worker);
+        }
+        return this;
+    }
+
+    public Behavior<ServerCommand> onCrash(ChildFailed fail) {
+        ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()), "worker_"+(workerIds++));
+        getContext().watch(worker);
+        idleWorkers.add(worker);
+        return this;
+    }
+
+}
